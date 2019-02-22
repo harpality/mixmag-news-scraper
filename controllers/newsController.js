6a1bd81613492a89a9ba57b3095a5e382b551161
Speedway <harpalassi@gmail.com>
2019-02-22 12:54:17 -0600

    initial webscrape working

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -8,8 +8,28 @@ const cheerio = require("cheerio");
 // Require all models
 // var db = require("../models");
 
+// GET route for index and scraping MixMag website
 router.get("/", (req, res) => {
-    res.render("index")
+    // grab the html body via axios
+    axios.get("https://mixmag.net/news").then((response) => {
+        // load the response into cheerio and save it to $
+        const $ = cheerio.load(response.data);
+        $("article").each((i, element) => {
+            let result = {};
+            result.title = $(element).find("h3").text();
+            result.summary = $(element).find("p").text();
+            result.link = `https://mixmag.net${$(element).find("a").attr("href")}`;
+            console.log(result);
+        });
+        
+        res.send("scrape complete")
+    })
+
+
+
+
+
+    // res.render("index")
   });
   
 
