ebc24ed6e786718bfef18ff836e062444490c97a
Speedway <harpalassi@gmail.com>
2019-02-23 00:51:17 -0600

    first comment appearing in modal

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -47,6 +47,46 @@ router.get("/scrape", (req, res) => {
         })
   });
   
+  router.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+          res.json(dbArticle);
+      })
+      .catch(function(err) {
+          res.json(err);
+      })
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("comment")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+router.post("/articles/:id", function(req,res) {
+    db.Comment.create(req.body)
+    .then(function(dbComment) {
+        return db.Article.findOneAndUpdate({_id:req.params.id}, {comment: dbComment._id}, {new:true});
+    })
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    })
+});
+
+
 
 
 module.exports = router;
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,14 +1,43 @@
 $(document).ready(function(){
     $('.modal').modal();
+
     $('.save-comment').click(function(){
         let thisId = $(this).attr('data-id'); 
         let comment = $(`#modal${thisId} #userComment`).val()
         console.log(comment, thisId)
         $(`#modal${thisId} #userComment`).val("");
+
+        $.ajax({
+            method: "POST",
+            url: "/articles/" + thisId,
+            data: {
+              body: comment
+            }
+          })
+            // With that done
+            .then(function(data) {
+              // Log the response
+              console.log(data);
+              // Empty the notes section
+            });
+
+    });
+
+    $('#open-comment').click(function() {
+        let thisId = $(this).attr("data-id");
+        console.log(thisId);
+        $.ajax({
+            method: "GET",
+            url: "/articles/" + thisId
+          })
+            .then(function(data) {
+              console.log(data);
+
+              if (data.comment) {
+                  $(`#modal${thisId} #comments`).text(data.comment.body)
+              }
+
+            })
     })
 
 });
-
-// $('#try').click(function() {
-//     $("#userComment").val("");
-// })
