cebb29093dc28f6766cbd767e4a7e33573207fe7
Speedway <harpalassi@gmail.com>
2019-02-22 20:35:43 -0600

    text input clearing after save

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $('.modal').modal();
-  });
-          
+    $('.save-comment').click(function(){
+        let comment = $("#userComment").val()
+        let thisId = $(this).attr('data-id'); 
+        console.log(comment, thisId)
+        $("#userComment").val("");
+    })
+
+});
