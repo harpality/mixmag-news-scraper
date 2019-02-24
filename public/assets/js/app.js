756877333dc4b1e337a0d79647312569bb20214a
Speedway <harpalassi@gmail.com>
2019-02-23 23:27:46 -0600

    disable form submit on modal

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,43 +1,38 @@
-$(document).ready(function(){
-    $('.modal').modal();
+$(document).ready(function() {
+  $(".modal").modal();
 
-    $('.save-comment').click(function(){
-        let thisId = $(this).attr('data-id'); 
-        let comment = $(`#modal${thisId} #userComment`).val()
-        console.log(comment, thisId)
-        $(`#modal${thisId} #userComment`).val("");
+  $(".save-comment").click(function(event) {
+    let thisId = $(this).attr("data-id");
+    let comment = $(`#modal${thisId} #userComment`).val();
+    console.log(comment, thisId);
+    $(`#modal${thisId} #userComment`).val("");
 
-        $.ajax({
-            method: "POST",
-            url: "/articles/" + thisId,
-            data: {
-              body: comment 
-            }
-          })
-            // With that done
-            .then(function(data) {
-              // Log the response
-              console.log(data);
-              location.reload();
-            
-            });
-
-    });
-
-    $('body').on("click", 'a.open-comment', function() {
-        let thisId = $(this).attr("data-id");
-        console.log(thisId);
-        $.ajax({
-            method: "GET",
-            url: "/articles/" + thisId
-          })
-            .then(function(data) {
-              console.log(data);
-              if (data.comment) {
-                  $(`#modal${thisId} #comments`).text(data.comment.body)
-              }
-
-            })
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        body: comment
+      }
     })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        location.reload();
+      });
+  });
 
+  $("body").on("click", "a.open-comment", function() {
+    let thisId = $(this).attr("data-id");
+    console.log(thisId);
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    }).then(function(data) {
+      console.log(data);
+      if (data.comment) {
+        $(`#modal${thisId} #comments`).text(data.comment.body);
+      }
+    });
+  });
 });
